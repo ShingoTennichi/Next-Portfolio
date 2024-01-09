4a5de96a28971eb8297e96dba3eab54eee988dcc
Shingo Tennichi <sngsng.ttt@gmail.com>
2024-01-09 12:44:51 -0800

    begins portfolio v2.0.0 project

---
+++ app/page.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <section>
+      <h1>new</h1>
+    </section>
+  )
+}
--- next.config.js
+++ next.config.js
@@ -1,21 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true,
-    fontLoaders: [
-      { loader: '@next/font/google', options: { subsets: ['latin'] } },
-    ],
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'raw.githubusercontent.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
