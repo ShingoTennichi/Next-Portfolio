0316dcc30ef94b762fc11f470b554ab3b7e7be2d
Shingo Tennichi <sngsng.ttt@gmail.com>
2024-06-27 10:53:35 -0700

    update favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -16,13 +16,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
-        <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#ffa812" />
-        <meta name="msapplication-TileColor" content="#ffa812" />
-        <meta name="theme-color" content="#ffffff" />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body className={lato.className}>
         <div className='wrapper'>
